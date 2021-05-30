d0950717c3780ded7f3bccc914ffcc35b041d2fd
toromuu <69118900+toromuu@users.noreply.github.com>
2021-05-30 13:28:23 +0200

    testSimple

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -6,9 +6,16 @@ import datetime
 
 class ViewTestCase(SimpleTestCase):
     
-    def testPost(self):
+    def testPostDate(self):
         p = Post(date=datetime.datetime.now(), photo=random_picture())
         d1 = p.date
         d2 = datetime.datetime.now()
-        self.assertTrue(abs(d1 - d2) < datetime.timedelta(seconds=1))
+        self.assertTrue(abs(d2 - d1) < datetime.timedelta(seconds=5))
         assert type(p.photo) is str
+
+    def testPostPhoto(self):
+        p = Post(date=datetime.datetime.now(), photo=random_picture())
+        assert type(p.photo) is str
+
+    def testModels(self):
+        pass
